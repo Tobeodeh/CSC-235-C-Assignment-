20e65df1c548e0838bd9b5aa5fb6aeaba62585e9
Tobechukwu Chris-Odeh <108990991+Tobeodeh@users.noreply.github.com>
2023-01-26 16:29:17 +0100

    Add files via upload

---
+++ AdamSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace AdamStepss
+{
+     
+    class Program
+    {
+        
+        public void counter(string steps)
+        {
+            /*This method checks each character of the inputted string and counts the number of times
+            the letter 'U' (which represents adam standing) appears in the string. it displays the value of count
+            whenever it would be called*/
+            int count = 0;
+            
+            foreach (int i in steps)
+            {
+                if (i == 'U')
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(count);
+        }   
+            
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of steps to check");
+            int no_times = int.Parse(Console.ReadLine());
+            for (int i = 0; i < no_times; i++)
+            {
+                Console.WriteLine("Enter the steps");
+                string steps = Console.ReadLine();
+                Program Steps = new Program();
+                Steps.counter(steps);
+            }
+           
+        }
+    }
+}
---
+++ self describing.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace selfdescribe
+{
+    class Program
+    {
+        private static int count;
+        private static int b;
+        static int sSelfdescribing(int num)
+        {
+            //This method counts the number of times a value equal to the specified index in the argument appears
+            string s = num.ToString();
+            for (int i = 0; i < s.Length; i++)
+            {
+                int count;
+                string temp = s[i] + "";
+                b = int.Parse(temp);
+
+                count = 0;
+                //creating another for loop to check 
+                for (int j = 0; j < s.Length; j++)
+                {
+                    int temp_value = int.Parse(s[j] + "");
+                    if (temp_value == i)
+                    {
+                        count++;
+                    }
+
+                }
+
+            }
+            return count;
+        }
+        static bool check(int num)
+        {
+            if (sSelfdescribing(num) != b)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        public void print(int num)
+        {
+            if (check(num) == true)
+            {
+                Console.WriteLine("Self Describing");
+            }
+
+            if (check(num) == false)
+            {
+                Console.WriteLine(" Not Self Describing");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of values to check: ");
+            int no_times = int.Parse(Console.ReadLine());
+            for (int i = 0; i < no_times; i++)
+            {
+                Console.WriteLine("Enter Number to check: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Program Test = new Program();
+                Test.print(num);
+            }
+        }
+    }
+}
